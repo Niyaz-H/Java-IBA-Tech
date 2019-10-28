93abeb206d9c79e1bf141be3d58730a7b9d76fbb
Niyaz-H <huseynzadehniyaz@gmail.com>
2019-10-28 20:09:52 +0400

    test commit

--- src/main/java/lesson08/warmup/SummarizerApp.java
+++ src/main/java/lesson08/warmup/SummarizerApp.java
@@ -9,6 +9,6 @@ public class SummarizerApp {
     Scanner in = new Scanner(System.in);
     String line = in.nextLine();
     int result = s.sum(line);
-    System.out.printf("The sum is:%d\n", result);
+    System.out.printf("The sum is: %d\n", result);
   }
 }
